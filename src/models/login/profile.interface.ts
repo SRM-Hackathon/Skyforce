bd0d07fd884e63f3faa78838c92f3e978445823c
AbiramaKrithika <umasiva979@gmail.com>
2018-10-13 04:34:20 +0530

    sign up update

--- src/models/login/profile.interface.ts
+++ src/models/login/profile.interface.ts
@@ -2,8 +2,8 @@ export interface profile{
     username : String;
     password : String;
     name : String;
-    email:String;
-    mobile:Number;
-    gender:String;
+    email:  String;
+    mobile: Number;
+    gender: String;
     
 }
