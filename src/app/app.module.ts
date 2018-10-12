7c26d0cbcb9687f0d65527c1cc90127e334ed940
dhivyasri97 <dhivyabalachandran97@gmail.com>
2018-10-12 19:37:48 +0530

    some update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,29 +8,27 @@ import { ListPage } from '../pages/list/list';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-<<<<<<< HEAD
+ HEAD
 import { SignupPage } from '../pages/signup/signup';
-=======
 import { SettingsPage } from '../pages/settings/settings';
 import { HelpPage } from '../pages/help/help';
 import { MyProfilePage } from '../pages/my-profile/my-profile';
 import { PlanridePage } from '../pages/planride/planride';
->>>>>>> 217b5623da70c87a6a28abd99b6eb9eda09c2a0d
+
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
     ListPage,
-<<<<<<< HEAD
-    SignupPage
-=======
+ HEAD
+    SignupPage,
     SettingsPage,
     HelpPage,
     MyProfilePage,
     PlanridePage
     
->>>>>>> 217b5623da70c87a6a28abd99b6eb9eda09c2a0d
+>>>>>>> 217b5623da70c87a6a28abd99b6
   ],
   imports: [
     BrowserModule,
