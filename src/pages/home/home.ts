acaa455ced9b04b102c6a6336f3bedea04564895
dhivyasri97 <dhivyabalachandran97@gmail.com>
2018-10-12 16:14:49 +0530

    login page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,16 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  username : string;
+  password : string;
   constructor(public navCtrl: NavController) {
 
   }
+  login()
+  {
+  console.log("username:"+this.username);
+  console.log("password:"+this.password);
+  }
+  
 
 }
