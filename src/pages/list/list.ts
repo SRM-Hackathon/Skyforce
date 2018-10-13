d6155e2286520c3f447ebc7f352727e5986d9c16
AbiramaKrithika <umasiva979@gmail.com>
2018-10-13 14:35:26 +0530

    prefinal

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -8,7 +8,9 @@ import { NavController, NavParams, DateTime } from 'ionic-angular';
 export class ListPage {
   selectedItem: any;
   icons: string[];
-  items: Array<{title: string, note: string, notes: string, icon: string}>;
+  BusNo: string[] = ["M19","B30","C23","C5","27G","52F","31B","5F","6G","2Y","39A","42A"];
+  Date: string[] = ["02/10/2018","01/10/2018","21/09/2018","31/08/2018","11/07/2018","11/06/2018","12/05/2018","05/05/2018","10/01/2018","26/12/2017","02/11/2017","15/08/2017"];
+  items: Array<{title: string, note: string, icon: string}>;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     // If we navigated to this page, we will have an item available as a nav param
@@ -21,9 +23,8 @@ export class ListPage {
     for (let i = 1; i < 11; i++) {
     
       this.items.push({
-        title: 'Bus No ' + i,
-        note: 'Date :' + i,
-        notes: 'Time ' + i,
+        title: this.BusNo[i],
+        note: this.Date[i],
         icon: 'bus'
       });
     }
